89d938f6f06b8707086ad5452dc339aa51332c8e
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-06-19 14:20:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,7 @@ class Student
         DB[:conn].execute(sql)
       end
 
-      def save
+      def save(name)
         sql = <<-SQL
         INSERT INTO students(name, grade)
           VALUES(?, ?)
