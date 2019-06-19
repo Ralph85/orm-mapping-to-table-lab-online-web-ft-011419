1a20265e080416cc161b7e499055b3a93afa911f
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-06-19 14:19:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,7 @@ class Student
         sql = <<-SQL
         INSERT INTO students(name, grade)
           VALUES(?, ?)
+          student.save
         SQL
 
         @id = DB[:conn].execute(sql, self.name, self.grade)
